567abf14e93d7dcc45c849c1ef65cfc9a027094f
sitikhopipah20 <skhopipah431@gmail.com>
2021-10-12 13:54:50 +0700

    first commit

---
+++ BiodataDiri.java
@@ -0,0 +1,50 @@
+public class BiodataDiri {
+  
+    public static void main(String[] args) {
+    
+        // String firstname;
+        // String lastname;
+        // String address;
+        // int age;
+        // string university;
+        // int nim;
+        // string course;
+        // String phonenumber;
+        // string hobby
+        // string favorite food;
+        // int weight;
+        // double height;
+    
+    
+        // firstname = "siti"
+        // lastname = "khopipah"
+        // address = "jalan bukit barisan 1 medan"
+        // age = "21 tahun"
+        // university = "Universitas muhammadiyah sumatera utara"
+        // nim = "1805170189"
+        // course = "akuntansi"
+        // phonenumber = "082272746737"
+        // hobby = "memasak"
+        // Favorit food = "mie ayam"
+        // weight = 58 kg;
+        // height = 163 cm;
+    
+    
+        System.out.println("firstname :" + "siti" );
+        System.out.println("lastname :" + "khopipah" );
+        System.out.println("address :" + "jalan bukit barisan 1 medan" );
+        System.out.println("age :" + "21 tahun" );
+        System.out.println("university :" + "universitas muhammadiyah sumatera utara" );
+        System.out.println("nim :" + "1805170189" );
+        System.out.println("course :" + "akuntansi" );
+        System.out.println("phonenumber :" + "082272746737" );
+        System.out.println("hobby :" + "memasak" );
+        System.out.println("favorite food :" + "mie ayam" );
+        System.out.println("weight :" + "58 kg" );
+        System.out.println("heigth :" + "163 cm" );
+     
+        
+    } 
+    
+    }
+    
